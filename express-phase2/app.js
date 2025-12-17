ff1f769ade3f2e8393ef7f015d667b5bd6aae171
jacky105402002 <JackyTsai@geo.com.tw>
2025-12-17 11:44:00 +0800

    Phase 2 Day 2：Express Middleware

--- express-phase2/app.js
+++ express-phase2/app.js
@@ -3,20 +3,46 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// 👇 全域 middleware
+app.use((req, res, next) => {
+  console.log(`[LOG] ${req.method} ${req.url}`);
+  next(); // 放行
+});
+
 app.get("/", (req, res) => {
   res.send("Hello from Express");
 });
 
 app.get("/time", (req, res) => {
-  res.json({
-    now: new Date().toISOString(),
-  });
+  res.json({ now: new Date().toISOString() });
 });
 
-app.get("/user", (req, res) => {
-  console.log(req.method); // GET
-  console.log(req.path); // /user
-  console.log(req.query); // ?a=1
+function checkApiKey(req, res, next) {
+  const apiKey = req.query.apiKey;
+
+  if (!apiKey) {
+    return res.status(401).json({ error: "Missing apiKey" });
+  }
+
+  next();
+}
+
+app.get("/secure", checkApiKey, (req, res) => {
+  res.json({ message: "You passed the middleware!" });
+});
+
+function mw1(req, res, next) {
+  console.log("mw1");
+  next();
+}
+
+function mw2(req, res, next) {
+  console.log("mw2");
+  next();
+}
+
+app.get("/chain", mw1, mw2, (req, res) => {
+  res.send("done");
 });
 
 app.listen(PORT, () => {
